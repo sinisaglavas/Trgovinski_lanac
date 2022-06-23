f39615396d211484677f316f743dbc9f63c53add
sinisaglavas <102516546+sinisaglavas@users.noreply.github.com>
2022-06-23 18:23:42 +0200

    Create food.php

---
+++ food.php
@@ -0,0 +1,11 @@
+<?php
+namespace src;
+
+/*
+ * 2. Kreirati radnju prehrambenih proizvoda koja ima 20 hlebova, po ceni od 50 dinara, 10 sokova po
+ceni od 100 dinara i 20 čipseva po ceni od 30 dinara.
+ */
+
+$hleb=[['kolicina'=>20,'cena'=>50]];
+$sok=[['kolicina'=>10,'cena'=>100]];
+$cips=[['kolicina'=>20,'cena'=>30]];
