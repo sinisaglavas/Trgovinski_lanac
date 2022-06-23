4c64a16a0c9967d2d61aaa5928abc14f5232a4ee
sinisaglavas <102516546+sinisaglavas@users.noreply.github.com>
2022-06-23 18:25:03 +0200

    Create pharmacy.php

---
+++ pharmacy.php
@@ -0,0 +1,8 @@
+<?php
+namespace src;
+/*
+ * 4.Kreirati radnju apoteka koja ima 20 lekova, po ceni od 1000 dinara i 50 sirupa po ceni od 50 dinara.
+ */
+
+$lekovi=[['kolicina'=>20,'cena'=>1000]];
+$sirupi=[['kolicina'=>50,'cena'=>50]];
