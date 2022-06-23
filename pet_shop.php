4aa24a24d65d363d64231393730e548ee70c1b03
sinisaglavas <102516546+sinisaglavas@users.noreply.github.com>
2022-06-23 18:24:17 +0200

    Create pet_shop.php

---
+++ pet_shop.php
@@ -0,0 +1,10 @@
+<?php
+namespace src;
+/*
+ * 3. Kreirati radnju proizvoda za kućne ljubimce koja ima 10 pakovanja granula, po ceni od 500 dinara,
+10 lekova po ceni od 1000 dinara i 15 igrački po ceni od 300 dinara.
+ */
+
+$granule=[['kolicina'=>10,'cena'=>500]];
+$lekovi=[['kolicina'=>10,'cena'=>1000]];
+$igracke=[['kolicina'=>15,'cena'=>300]];
