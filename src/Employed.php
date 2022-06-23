432cc00b83c9bcd75da6797626de402b095dbdb9
sinisaglavas <102516546+sinisaglavas@users.noreply.github.com>
2022-06-23 18:17:09 +0200

    Create Employed.php

---
+++ src/Employed.php
@@ -0,0 +1,16 @@
+<?php
+namespace src;
+
+class Employed
+{
+    protected $name='Pera';
+    protected $last_name='Peric';
+    protected $store='Food';
+
+public function buy($product){
+
+}
+
+
+
+}
